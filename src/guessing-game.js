0b25c5f88a02d5b1aa23991cd6075d258d796216
Alex89889 <mihal4encko.sasha@yandex.ru>
2019-10-13 00:16:28 +0300

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.minRange;
+		this.maxRange;
+		this.solution;
+	}
 
     setRange(min, max) {
-
+		this.minRange = min;
+        this.maxRange = max;
     }
 
     guess() {
-
+		this.solution = Math.round((this.minRange + this.maxRange) / 2);
+		return this.solution;
     }
 
     lower() {
-
+        this.maxRange = this.solution;
     }
 
     greater() {
-
+        this.minRange =this.solution;
     }
 }
 
